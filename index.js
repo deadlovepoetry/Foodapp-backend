fa62044c443ce8bd4e044a977377881eb5a5c70e
Anamitra <anamitrahazra88915@gmail.com>
2024-07-29 23:59:20 +0530

    cors enabled

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 const mongoDB = require('./db');
 mongoDB();
 
-// Manually set CORS headers
+// Use CORS middleware
+app.use(cors());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
