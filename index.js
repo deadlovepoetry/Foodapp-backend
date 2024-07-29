5c6251975c68f6eb5a1ceaf18a7710cacce229f9
Anamitra <anamitrahazra88915@gmail.com>
2024-07-30 00:03:56 +0530

    cors enabled1

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ mongoDB();
 // Use CORS middleware
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
     
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
+//     // Handle preflight requests
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
     
-    next();
-});
+//     next();
+// });
 
 // Middleware to parse JSON bodies
 app.use(express.json());
